05b0b927f23e48a14fd63a9f737db186c0ace3a3
Abhi <138367906+Parad0x-143@users.noreply.github.com>
2023-12-07 20:58:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.getElementById("button");
   button.classList.add("hidden");
   button.classList.remove("button");
 }
+
+function myFunction1() {
+  const element = document.getElementById("sketch");
+  const button =
+document.getElementById("button");
+  element.classList.add("hidden");
+  button.classList.remove("hidden");
+  button.classList.add("button");
+}
