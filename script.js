c910d8d0546c6f29abbf047e339585836c425926
Abhi <138367906+Parad0x-143@users.noreply.github.com>
2023-12-09 01:34:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ document.getElementById("button");
   button.classList.remove("hidden");
   button.classList.add("button");
 }
+
+const element = document.querySelector(".menu");
+
+const menubtn =
+document.querySelector(".menu-button-bars")
+
+const sidebar=
+document.querySelector(".sidebar")
+const sidebar2=
+document.querySelector(".sidebar2")
+const sidebar3=
+document.querySelector(".sidebar3")
+const on =
+document.querySelector(".on")
+
+element.addEventListener('click', () => {    element.classList.toggle("active"); 
+   sidebar.classList.toggle("active");
+   sidebar2.classList.toggle("active");
+   sidebar3.classList.toggle("active");
+   on.classList.toggle("turnoff");
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+// For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
